7b2acaed3f49bcf9e0425a945b3039bfee2062dd
Rocket-Octane <93807708+Rocket-Octane@users.noreply.github.com>
2022-08-04 22:41:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-song = "";
+song1 = "";
+song2 = "";
 
 function preload(){
-    song = loadSound("")
+    song = loadSound("music1.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(665, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 665, 500);
 }
