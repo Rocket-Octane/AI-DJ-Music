5e7895638d1b934d4f40f31fbe9d4741cc9af0ab
Rocket-Octane <93807708+Rocket-Octane@users.noreply.github.com>
2022-08-09 15:48:06 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
     song1 = loadSound("music1.mp3");
@@ -12,6 +16,25 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Left Wrist X = " + leftWristX + "     Left Wrist Y = " + leftWristY);
+        console.log("Right Wrist X = " + rightWristX + "     Right Wrist Y = " + rightWristY);
+    }
 }
 
 function draw(){
